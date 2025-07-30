6942f0caf77f636b241484184909737f8857bf85
Ahad-CMA <ahadkhot96@gmail.com>
2025-07-30 16:57:49 +0530

    Challenge 11 added

---
+++ css-battle/src/challenges.jsx
@@ -0,0 +1,124 @@
+function Challenges(){
+return (
+    <>
+<h1 style={{ textAlign: "center" }}>CSS BATTLE</h1>
+<div className="flex">
+  <div className="challange1">
+    <div className="main">
+      <div className="c"></div>
+      <div className="out">
+        <div className="in"><div className="red"></div></div>
+      </div>
+    </div>
+  </div>
+
+  {/* <!-- CHALLENGE 2 --> */}
+  <div className="challenge2">
+    <div className="cont">
+      <div className="b1"></div>
+      <div className="b2"></div>
+      <div className="b3"></div>
+      <div className="b5"></div>
+    </div>
+    <div className="cont2">
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+
+  {/* <!-- CHALLENGE 3 --> */}
+  <div className="challenge3">
+    <div className="circle"></div>
+    <div className="box"></div>
+    <div className="box b2"></div>
+  </div>
+
+  {/* <!-- CHALLENGE 4 --> */}
+  <div className="challenge4">
+    <div className="container">
+      <div className="row1">
+        <div></div><div></div><div></div>
+      </div>
+      <div className="row2">
+        <div></div><div></div><div></div>
+      </div>
+      <div className="row1">
+        <div></div><div></div><div></div>
+      </div>
+      <div className="row2">
+        <div></div><div></div><div></div>
+      </div>
+      <div className="row1">
+        <div></div><div></div><div></div>
+      </div>
+    </div>
+  </div>
+
+  {/* <!-- CHALLENGE 5 --> */}
+  <div className="challenge5">
+    <div>
+      <div className="circ"></div>
+      <div className="smcirc"></div>
+      <div className="white"></div>
+      <div className="mwhite"></div>
+    </div>
+  </div>
+
+  {/* <!-- CHALLENGE 6 --> */}
+  <div className="challenge6">
+    <div className="c"></div>
+    <div className="p">
+      <div className="d"></div>
+      <div className="d l"></div>
+      <div className="d l"></div>
+    </div>
+    <div className="b c"></div>
+  </div>
+
+  {/* <!-- CHALLENGE 7 --> */}
+  <div className="challenge7">
+    <div className="m br bc">
+      <div className="n br ab bb">
+        <div className="o br ab bc">
+          <div className="p br ab bb"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  {/* <!-- CHALLENGE 8 --> */}
+  <div className="challenge8">
+    <div className="bl"></div>
+    <div className="wh"></div>
+    <div className="bl b"></div>
+    <div className="wh w"></div>
+  </div>
+
+  {/* <!-- CHALLENGE 9 --> */}
+  <div className="challenge9">
+    <div className="c1"></div>
+    <div className="c2"></div>
+    <div className="c3"></div>
+  </div>
+
+  {/* <!-- CHALLENGE 10 --> */}
+  <div className="challenge10">
+    <div className="a"></div>
+    <div className="c"></div>
+    <div className="e c"></div>
+    <div className="b"></div>
+    <div className="d c"></div>
+  </div>
+
+  {/* <!-- CHALLENGE 11 --> */}
+  <div className="challenge11">
+    <div className="a d"></div>
+    <div className="b d"></div>
+    <div className="c"></div>
+  </div>
+</div>
+</>
+);
+
+}
+export  default Challenges;
